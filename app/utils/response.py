5c721f6f8e7aae4cf6cfc3174086e86398b04c06
nplaybook <naufal.abdila@outlook.com>
2022-03-30 16:12:05 +0700

    create functions that handle response tempalte

---
+++ app/utils/response.py
@@ -0,0 +1,24 @@
+from typing import Dict, Any, Optional, List
+
+
+def success_response(message: str, data: List[Dict[str, Any]] = []) -> Dict[str, Any]:
+    """Generate template for success response
+    
+    :param message: simple message that tells request success
+    :param data: data that will be delivered
+    :return: dictionary that will be used as success response
+    """
+
+    if data is None:
+        data = []
+    return { "result": "Success", "message": message, "data": data }
+
+def error_response(message: str, err: str) -> Dict[str, Any]:
+    """Generate template for success response
+    
+    :param message: simple message that tells request fails
+    :param err: error message that will be delivered
+    :return: dictionary that will be used as fail response
+    """
+
+    return { "result": "Fail", "message": message, "err": err, "data": [] }
