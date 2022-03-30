9856f70753a0784e2298e93d5a8369b09223b64c
nplaybook <naufal.abdila@outlook.com>
2022-03-31 06:48:04 +0700

    create additional validator in email subject and content

--- app/schemas/email.py
+++ app/schemas/email.py
@@ -1,18 +1,27 @@
 from typing import List
-from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
-
+from pydantic import BaseModel, EmailStr, Field, validator
+from pydantic.types import constr
 
 class EmailBase(BaseModel):
     event_id: int
     email_subject: str
     email_content: str
     from_email: EmailStr 
-    schedule_at: datetime = Field(alias="timestamp")
 
-    class Config:
-        orm_mode = True
+
+    @validator("email_subject")
+    def check_email_subject(cls, v):
+        assert v != "", "Empty subject is not allowed"
+        return v
+
+
+    @validator("email_content")
+    def check_email_content(cls, v):
+        assert v != "", "Empty content is not allowed"
+        return v
 
 
 class SaveEmailPayload(EmailBase):
     to_email: List[EmailStr]
+    schedule_at: datetime = Field(alias="timestamp")
