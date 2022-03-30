06119c323d7cf5f15f1d023ed35114fe56f5990f
nplaybook <naufal.abdila@outlook.com>
2022-03-30 16:14:25 +0700

    add constraint and minor column naming

--- app/models/email.py
+++ app/models/email.py
@@ -10,12 +10,12 @@ from .base import Base
 class Email(Base):
     __tablename__ = "emails"
     id = Column(Integer, primary_key=True, nullable=False)
-    event_id = Column(Integer, ForeignKey("events.id"))
-    subject = Column(String)
-    content = Column(String)
-    from_email = Column(String)
-    schedule_at = Column(DateTime)
-    status = Column(String(16))
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+    email_subject = Column(String, nullable=False)
+    email_content = Column(String, nullable=False)
+    from_email = Column(String, nullable=False)
+    schedule_at = Column(DateTime, nullable=False)
+    status = Column(String(16), nullable=False)
     created_at = Column(DateTime(), default=datetime.now)
     updated_at = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
     recipients = relationship("Recipient")
