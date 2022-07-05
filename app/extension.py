7e8c81ce7f85755c94c0b8f211c667ed6a242954
nplaybook <naufal.abdila@outlook.com>
2022-07-05 10:57:45 +0700

    create celery intializer function

--- app/extension.py
+++ app/extension.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from celery import Celery
 
-from app.config import DB_BASE_DIR, DB_DIALECT, DB_NAME
+from app.config import (
+    DB_BASE_DIR, DB_DIALECT, DB_NAME,
+    CELERY_BROKER_URL, CELERY_RESULT_BACKEND,
+    CELERY_ACCEPT_CONTENT, CELERY_RESULT_SERIALIZER
+    # CELERY_TASK_SERIALIZER
+)
 
 
 def init_engine():
@@ -15,3 +21,19 @@ def init_session():
     """Create sqlalchemy session"""
 
     return sessionmaker()
+
+
+def init_celery():
+    """Create celery worker"""
+
+    celery = Celery(
+        "tasks",
+        broker=CELERY_BROKER_URL,
+        backend=CELERY_RESULT_BACKEND
+        )
+    celery.conf.update(
+        # task_serializer=CELERY_TASK_SERIALIZER,
+        accept_content=CELERY_ACCEPT_CONTENT,
+        result_serializer=CELERY_RESULT_SERIALIZER
+    )
+    return celery
