e4330cf32f735b95e56bd0d53fbe5e1848ccfc42
nplaybook <naufal.abdila@outlook.com>
2022-07-05 17:04:13 +0700

    Revert "update const variable"
    This reverts commit 5c61170f4e65596f913a7d216c657e3baab725f5.

--- app/library/const.py
+++ app/library/const.py
@@ -1,5 +1,3 @@
-import os
-
 # Email essentials
 PAYLOAD_EMAIL = ["event_id", "from_email", "email_subject", "email_content", "schedule_at"]
 PAYLOAD_RECIPIENT = "to_email"
@@ -11,6 +9,3 @@ STATUS_SENT = "SENT"
 
 # Email schedule
 SCHEDULE_LIMIT = 60 * 2
-
-# Export base path
-EXPORT_BASE_PATH = f"{os.getcwd()}/"
