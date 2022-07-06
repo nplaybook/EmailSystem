db27d91dc4a24e40c6f2b2c23d28b1b80cc1b233
nplaybook <naufal.abdila@outlook.com>
2022-07-06 19:46:41 +0700

    create method to handle excel file generator

--- app/library/backend_scripts.py
+++ app/library/backend_scripts.py
@@ -1,9 +1,11 @@
-from typing import NoReturn, List
+from typing import List
 from datetime import datetime, timezone
-from app.library.const import STATUS_SENT
-from app.library.typing import DB_SESSION
+
+from openpyxl import Workbook
 
 from app.models import Email, Recipient
+from app.library.const import STATUS_SENT
+from app.library.typing import DB_SESSION
 
 
 class DBEmailTransaction:
@@ -54,7 +56,7 @@ class ExcelFileHandler:
     :param table_name (str): name of the table that will be exported
     """
 
-    def __init__(self, table_name: str) -> NoReturn:
+    def __init__(self, table_name: str):
         """Constructor for ExcelFileHandler
 
         :param table_name (str): name of the table taht will be exported
@@ -62,18 +64,56 @@ class ExcelFileHandler:
 
         self.table_name = table_name
 
-    def generate_filename(self) -> str:
+    def generate_filename(self, time_format: str, ext: str) -> str:
         """Auto generate excel filename following specific format
 
-        :param table: name of the table that will be reported
+        :param format: desired time format
+        :param ext: file extension
         :return: auto generated excel filename
         """
 
-        time_generated = datetime.now(timezone.utc).strftime("%d %b %Y, %H:%M")
-        return f"{self.table_name} - {time_generated}.xlsx"
+        time_generated = datetime.now(timezone.utc).strftime(time_format)
+        return f"{self.table_name} - {time_generated}.{ext}"
 
-    def remove_existing_excel_file(self, base_path: str) -> NoReturn:
+    def remove_existing_excel_file(self, base_path: str):
         pass
 
-    def generate_excel_file(self, base_path: str):
-        pass
+    def generate_excel_file(self, base_path: str, file_name: str, header: str, data: list) -> Workbook:
+        """Generate new excel file
+        
+        :param base_path: path where the excel file will be stored
+        :param file_name: name of the file
+        :param header: data header
+        :param data: data that will be written to the excel file
+        :return: excel Workbook
+        """
+
+        workbook = Workbook()
+        workbook = self.write_data_to_excel(workbook, header, data)
+        workbook.save(f"{base_path}/{file_name}")
+
+    @staticmethod
+    def write_data_to_excel(workbook: Workbook, header: list, data: list) -> Workbook:
+        """Write data to excel
+        
+        :param workbook: empty workbook
+        :param header: data header
+        :param data: data that will be written
+        """
+
+        wb_active = workbook.active
+
+        # add header
+        wb_active.append(header)
+
+        # format data dynamically following available header
+        formatted_data = []
+        for datum in data:
+            row_data = [getattr(datum, head) for head in header]
+            formatted_data.append(row_data)
+
+        # insert data to excel file
+        for datum in formatted_data:
+            wb_active.append(datum)
+
+        return workbook
