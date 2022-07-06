4a9fdfaff79b3ff34af7b937303517c431a6a66f
nplaybook <naufal.abdila@outlook.com>
2022-07-06 19:45:45 +0700

    create get column name method

--- app/library/orm.py
+++ app/library/orm.py
@@ -6,7 +6,7 @@ from app.library.typing import JSON, JSON_ARRAY
 
 class OrmUtils:
 
-    def __init__() -> NoReturn:
+    def __init__():
         pass
 
     @staticmethod
@@ -29,3 +29,17 @@ class OrmUtils:
         """
 
         return [OrmUtils.serialize(obj) for obj in objs]
+
+    @staticmethod
+    def get_columns_name(objs: List[ModelBase], many: bool) -> List[str]:
+        """Get query's column name. Can be done by accessing one of the
+        item in result's list
+        
+        :param objs: list of orm model
+        :param many: True if it contains multiple columns
+        :return: list of column names
+        """
+
+        if many:
+            return [column.key for column in objs[0].__table__.columns]
+        return list(objs[0].keys())
